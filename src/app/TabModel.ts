8ea5c1234da6f6446081ee164c151cda4e6d73aa
Sara Li <slli@spscommerce.com>
2019-03-12 21:10:28 -0400

    Created tab model and updated app component

---
+++ src/app/TabModel.ts
@@ -0,0 +1,7 @@
+export class TabModel {
+  title: string;
+  url: string
+  favIconUrl: string;
+  windowId: number;
+  tabIndex: number;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, NgZone } from '@angular/core';
+import { TabModel } from './TabModel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-chrome-ext';
+export class AppComponent implements OnInit, OnChanges {
+  ngOnChanges(changes: import("C:/git/angular-chrome-ext/node_modules/@angular/core/src/metadata/lifecycle_hooks").SimpleChanges): void {
+      throw new Error("Method not implemented.");
+  }
+
+  public title = 'The Angular Chrome Extension';
+  public tabList: TabModel[];
+
+  constructor(
+    private zone: NgZone
+  ) { }
+
+  ngOnInit(): void {
+    this.tabList = [];
+
+    this.zone.run(() => {
+      this.getTabList();
+    });
+  }
+
+  getTabList(): void {    
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+      tabs.forEach((tab) => {
+        console.log('Tab URL: ', tab.url);
+
+        this.tabList.push({
+          title: tab.title,
+          url: tab.url,
+          favIconUrl: tab.favIconUrl,
+          windowId: tab.windowId,
+          tabIndex: tab.index
+        });
+      });
+    });
+  };
 }
+
+
